ee6b312e85d1456898a58471fec7b42abb4f4f68
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-03-31 15:55:20 +0100

    .

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -3,10 +3,19 @@ import React, { Component } from 'react';
 class Counter extends Component {
     componentDidUpdate(prevProps, prevState){
         console.log('prevProps',prevProps);
-        console.log('prevState',prevState)
+        console.log('prevState',prevState);
+        if (prevProps.counter.value !== this.props.counter.value) {
+            // Ajax call and get new data from the server
+        }
+    }
+
+    componentWillUnmount() {
+        console.log('Counter - Unmount');
     }
 
     render() { 
+        console.log('Counter - Rendered');
+        
         return (
           <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
